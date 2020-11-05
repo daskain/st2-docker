fe9a29cdaa71266df95038285e74110d59a0ad38
daskain <daskain@gmail.com>
2020-11-05 17:54:28 +0000

    fix project

--- scripts/chatops.sh
+++ scripts/chatops.sh
@@ -1,8 +1,9 @@
-!/usr/bin/env bash
+#!/usr/bin/bash
 
+chown -R 1000:1000 ./scripts/
 export HUBOT_ADAPTER=slack
 # Obtain the Slack token from your app page at api.slack.com, it's the "Bot
 # User OAuth Access Token" in the "OAuth & Permissions" section.
-export HUBOT_SLACK_TOKEN=xoxb-CHANGE-ME-PLEASE
+export HUBOT_SLACK_TOKEN=xoxb-1454377019200-1482889937617-ZyHbphd6jfrKOEjcHA4kkQeO
 # Uncomment the following line to force hubot to exit if disconnected from slack.
 export HUBOT_SLACK_EXIT_ON_DISCONNECT=1
